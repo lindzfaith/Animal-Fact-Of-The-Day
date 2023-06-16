0cead87688eb91bae7f61d45117c8d8f7c550d13
Lindsey Dunkley <lindsey.dunkley@pearson.com>
2023-06-16 18:24:51 -0400

    Fixed button

--- src/Features/Fact/Fact.tsx
+++ src/Features/Fact/Fact.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Fact.module.css";
 
 function Fact(props) {
+  const [i, setI] = useState(0);
   return (
     <div className={styles.fact}>
       <h1>{props.fact.name}</h1>
       {props.fact.component}
-      <p>{props.fact.facts[0]}</p>
+      <p>{props.fact.facts[i]}</p>
+      <button
+        onClick={() =>
+          setI((i) => (i < props.fact.facts.length - 1 ? i + 1 : 0))
+        }
+      >
+        Next Fact
+      </button>
     </div>
   );
+}
+
+function functionIncrement() {
+  i++;
 }
 
 export default Fact;
--- src/facts.js
+++ src/facts.js
@@ -6,6 +6,8 @@ export const facts = [
     component: <Cat />,
     facts: [
       "On average, cats spend around two-thirds of each day asleep. The average 12-year-old cat has been awake for only 4 years.",
+      "Cats can jump 5 times their own height.",
+      "House cats share 95.6% of their genetic makeup with tigers.",
     ],
   },
   {
